803d9203f95aa79f102044b5c2b41722faf65835
yaohaijiao <yaohaijiao@gaosiedu.com>
2018-11-05 22:49:21 +0800

    enable move function

--- src/Bus.js
+++ src/Bus.js
@@ -1,12 +1,54 @@
+class Direction {
+    constructor(initDirection){
+        this.current = initDirection;
+        this.map = {
+            'NORTH': new Position(0, 1),
+            'EAST': new Position(1, 0),
+            'SOUTH': new Position(0, -1),
+            'WEST': new Position(-1, 0)
+        };
+    }
+
+    move(position){
+        const offset = this.map[this.current];
+        return position.move(offset);
+    }
+
+    asString(){
+        return this.current;
+    }
+}
+module.exports = Direction;
+
+class Position {
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
+
+    move(offset){
+        this.x += offset.x;
+        this.y += offset.y
+        return this;
+    }
+}
+module.exports = Position;
 class Bus {
     constructor(commands){
-        const initDirection = commands.split(' ')[1].split(',');
-        this.x = parseInt(initDirection[0]);
-        this.y = parseInt(initDirection[1]);
-        this.direction = initDirection[2];
+        let lines = commands.split('\n');
+
+        const initDirection = lines.shift().split(/ /g)[1].split(',');
+        this.position = new Position(parseInt(initDirection[0]), parseInt(initDirection[1]));
+        this.direction = new Direction(initDirection[2]);
+
+        lines.forEach((command) => {
+            if('move' === command.toLowerCase()){
+                this.position = this.direction.move(this.position);
+            }
+        });
     }
     report(){
-        return `${this.x},${this.y},${this.direction}`;
+        return `${this.position.x},${this.position.y},${this.direction.asString()}`;
     }
 }
 
--- test/Bus.js
+++ test/Bus.js
@@ -5,11 +5,18 @@ describe('bus', function() {
   it('should be able to report current state after initialization', function() {
     const input1 = 'PLACE 0,0,NORTH';
     const input2 = 'PLACE 1,2,SOUTH';
-    let bus1 = new Bus(input1);
-    let bus2 = new Bus(input2);
+    const bus1 = new Bus(input1);
+    const bus2 = new Bus(input2);
     
     assert.equal(bus1.report(), '0,0,NORTH');
     assert.equal(bus2.report(), '1,2,SOUTH');
   });
+
+  it('should be able to move after initialization', function() {
+    const input = 'PLACE 0,0,NORTH\nMOVE';
+    const bus = new Bus(input);
+
+    assert.equal(bus.report(), '0,1,NORTH');
+  })
 
 });
